96e210e97a184622b2028e5876dd551c6e3cc7a0
sbin0819 <sbinha123@gmail.com>
2021-02-08 18:53:07 +0900

    feat: 기본세팅

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  font-size: 12px;
+  color: gray;
+`;
+
+const index: React.FC = () => {
+  return <Container>hello world</Container>;
+};
+
+export default index;
