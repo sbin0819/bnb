03595dee61ee2ac60c42293d8c4492f59eed86ad
sbin0819 <sbinha123@gmail.com>
2021-02-13 18:05:55 +0900

    feat: signup modal

---
+++ components/common/Selector.tsx
@@ -0,0 +1,73 @@
+/* eslint-disable indent */
+import styled, { css } from 'styled-components';
+import palette from '../../styles/palette';
+
+import { useSelector } from '../../store';
+
+const Container = styled.div<{ isValid: boolean; validateMode: boolean }>`
+  width: 100%;
+  height: 46px;
+
+  select {
+    width: 100%;
+    height: 100%;
+    background-color: white;
+    border: 1px solid ${palette.gray_eb};
+    padding: 0 11px;
+    border-radius: 4px;
+    outline: none;
+    -webkit-appearance: none;
+    background-image: url('/static/svg/common/selector/selector_down.svg');
+    background-position: right 11px center;
+    background-repeat: no-repeat;
+    font-size: 16px;
+
+    &:focus {
+      border-color: ${palette.dark_cyan};
+    }
+  }
+
+  ${({ isValid, validateMode }) =>
+    validateMode &&
+    css`
+      select {
+        border-color: ${isValid ? palette.dark_cyan : palette.tawny} !important;
+
+        background-color: ${isValid ? 'white' : palette.snow};
+      }
+    `}
+`;
+
+interface IProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
+  options?: string[];
+  disabledOptions?: string[];
+  value?: string;
+  isValid?: boolean;
+}
+
+const Selector: React.FC<IProps> = ({
+  options = [],
+  disabledOptions = [],
+  isValid,
+  ...props
+}) => {
+  const validateMode = useSelector((state) => state.common.validateMode);
+  return (
+    <Container isValid={!!isValid} validateMode={validateMode}>
+      <select {...props}>
+        {disabledOptions.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
+        {options.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </Container>
+  );
+};
+
+export default Selector;
