425c84289ab06b9edb5e9809f874e541074fc146
sbin0819 <sbinha123@gmail.com>
2021-02-14 12:21:36 +0900

    feat: add getInitialProps for cookie

--- lib/data/user.ts
+++ lib/data/user.ts
@@ -23,4 +23,10 @@ const write = async (users: StoredUserType[]) => {
   writeFileSync('lib/data/users.json', JSON.stringify(users));
 };
 
-export default { getList, exist, write };
+//* email의 유저 불러우기
+const find = ({ email, id }: { email?: string; id?: number }) => {
+  const users = getList();
+  return users.find((user) => user.email === email || user.id === id);
+};
+
+export default { getList, exist, write, find };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import { AppProps } from 'next/app';
+import App, { AppContext, AppProps } from 'next/app';
 import GlobalStyle from '../styles/GlobalStyle';
 import Header from '../components/Header';
 
 import { wrapper } from '../store';
+import { cookieStringToObject } from '../lib/utils';
+
+import axios from 'axios';
+import { meAPI } from '../lib/api/auth';
 
 const app = ({ Component, pageProps }: AppProps) => {
   return (
@@ -13,6 +17,28 @@ const app = ({ Component, pageProps }: AppProps) => {
       <div id="root-modal" />
     </>
   );
+};
+
+app.getInitialProps = async (context: AppContext) => {
+  const appInitialProps = await App.getInitialProps(context);
+  const cookieObject = cookieStringToObject(context.ctx.req?.headers.cookie);
+  const { store } = context.ctx;
+  const { isLogged } = store.getState().user;
+  try {
+    if (!isLogged && cookieObject.access_token) {
+      // axios.defaults.headers.cookie = cookieObject.access_token;
+      // ! not working
+      // const {data} = await meAPI();
+      const { data } = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/auth/me`
+      );
+      console.log(data);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  return { ...appInitialProps };
 };
 
 export default wrapper.withRedux(app);
